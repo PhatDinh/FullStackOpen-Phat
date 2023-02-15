bae1142e29bb79856bac4ebe07981f461a957e6a
PhatDinh <46390044+PhatDinh@users.noreply.github.com>
2023-02-15 11:04:09 +0700

    anecdotes step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -35,10 +35,14 @@ const App = () => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
       <button onClick={voteAnecdote}>vote</button>
       <button onClick={getNewAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[points.indexOf(Math.max(...points))]}</p>
+      <p>has {points[points.indexOf(Math.max(...points))]} votes</p>
     </div>
   )
 }
